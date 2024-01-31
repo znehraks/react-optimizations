751d9d85f59b10d4566fb58f008cf5a621f79874
znehraks <znehraks@gmail.com>
2024-02-01 01:56:21 +0900

    custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import classNames from 'classnames/bind'
 import styles from './App.module.scss'
 import FullScreenMessage from '@shared/FullScreenMessage'
 import Heading from '@components/sections/Heading'
 import Video from '@components/sections/Video'
-import { Wedding } from './models/wedding'
 import ImageGallery from './components/sections/ImageGallery'
 import Intro from './components/sections/Intro'
 import Invitation from './components/sections/Invitation'
@@ -14,34 +13,13 @@ import Map from './components/sections/Map'
 import Contact from './components/sections/Contact'
 import Share from './components/sections/Share'
 import AttendCountModal from './components/AttendCountModal'
+import useWedding from './hooks/useWedding'
 
 const cx = classNames.bind(styles)
 function App() {
-  const [wedding, setWedding] = useState<Wedding | null>(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
+  const { wedding, loading, error } = useWedding()
   const [count, setCount] = useState(0)
-  useEffect(() => {
-    setLoading(true)
 
-    fetch('http://localhost:8888/wedding')
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error('청첩장 정보를 불러오지 못했습니다.')
-        }
-        return res.json()
-      })
-      .then((data) => {
-        setWedding(data)
-      })
-      .catch((e) => {
-        console.log(e)
-        setError(true)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }, [])
   if (loading) {
     return <FullScreenMessage type="loading" />
   }
